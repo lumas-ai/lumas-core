dc4aee1996b1ff2b9c0b6e4777aaba1e0a79a7c5
Carl Caum <carl@carlcaum.com>
2019-02-25 20:05:31 -0800

    Remove erroneous files

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build --network host -f Dockerfile.opencv --no-cache -t lumas/lumas-python-opencv:latest .
-docker build --network host -f controller/Dockerfile --no-cache -t lumas/lumas-controller:latest .
-docker build --network host -f object-detection/Dockerfile --no-cache -t lumas/lumas-object-detection:latest .
