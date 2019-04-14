267e5ae80076fd2be37c66d3c9ad3d1873d19973
Carl Caum <carl@carlcaum.com>
2019-04-13 18:19:03 -0700

    Initial stream and source services
    This commit introduces the stream server and source server. The stream
    server handles incoming stream requests, including requests to record
    video.
    
    The camera providers will issue a `record` RPC call and get back a
    session. The session information will tell them where to stream their
    video and audio.
    
    Camera players (extensions that can stream video) and extensions that
    need to stream video from events both call the stream API to set up a
    stream session which includes the RPC address and ports to send the
    video and audio.
    
    The source server handles mapping if a source is from an event or from a
    camera directly.

---
+++ streamserver/cmd/stream-server/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	api "github.com/lumas-ai/lumas-core/protos/golang/stream"
+	"github.com/lumas-ai/lumas-core/streamserver"
+)
+
+var (
+	tls         = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	certFile    = flag.String("cert_file", "", "The TLS cert file")
+	keyFile     = flag.String("key_file", "", "The TLS key file")
+	iface       = flag.String("host", "0.0.0.0", "The interface to listen on")
+	redisServer = flag.String("redis_server", "redis:6379", "The Redis server address in format ip:port")
+	redisPass   = flag.String("redis_pass", "", "The Redis password")
+	port        = flag.Int("port", 5391, "The port to listen on")
+)
+
+func main() {
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", *iface, *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s, _ := streamserver.Init(*redisServer, *redisPass)
+
+	grpcServer := grpc.NewServer()
+	api.RegisterStreamServer(grpcServer, s)
+	grpcServer.Serve(lis)
+}
---
+++ streamserver/stream_server.go
@@ -0,0 +1,161 @@
+package streamserver
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+
+	"github.com/go-redis/redis"
+	"github.com/google/uuid"
+	api "github.com/lumas-ai/lumas-core/protos/golang/stream"
+	"github.com/nitishm/go-rejson"
+)
+
+type StreamServer struct {
+	redis *rejson.Handler
+}
+
+func Init(redisServer string, redisPass string) (*StreamServer, error) {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     redisServer,
+		Password: redisPass,
+		DB:       0,
+	})
+
+	pong, err := redisClient.Ping().Result()
+	if pong != "PONG" || err != nil {
+		return nil, err
+	}
+
+	redisJSONClient := rejson.NewReJSONHandler()
+	redisJSONClient.SetGoRedisClient(redisClient)
+
+	return &StreamServer{
+		redis: redisJSONClient,
+	}, nil
+}
+
+func result(errorKind string, errorMessage string) *api.Result {
+	if errorKind == "" && errorMessage == "" {
+		return &api.Result{
+			Successful: true,
+		}
+	}
+	return &api.Result{
+		Successful: false,
+		ErrorKind:  errorKind,
+		Message:    errorMessage,
+	}
+}
+
+func (s *StreamServer) delSession(session *api.Session) error {
+	if _, exists := s.getSession(session.Id); !exists {
+		errMsg := fmt.Sprintf("Could not find session with id %s", session.Id)
+		return errors.New(errMsg)
+	}
+
+	res, err := s.redis.JSONDel("session_"+session.Id, ".")
+	if res.(string) != "OK" || err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *StreamServer) setSession(session *api.Session) error {
+	res, err := s.redis.JSONSet("session_"+session.Id, ".", session)
+	if res == nil || err != nil {
+		errMsg := fmt.Sprintf("Could not set session to Redis: %v", err.Error())
+		log.Println(errMsg)
+		return err
+	}
+
+	return nil
+}
+
+func (s *StreamServer) getSession(id string) (*api.Session, bool) {
+	var sessionJSON interface{}
+	var err error
+	session := &api.Session{}
+
+	if sessionJSON, err = s.redis.JSONGet("session_"+id, "."); err != nil {
+		errMsg := fmt.Sprintf("Unable to retrieve session from Redis: %s", err.Error())
+		log.Println(errMsg)
+		return session, false
+	}
+
+	if sessionJSON == nil {
+		return session, false
+	}
+
+	if err := json.Unmarshal(sessionJSON.([]byte), &session); err != nil {
+		errMsg := fmt.Sprintf("Could not retrieve session from Redis: %v", err.Error())
+		log.Println(errMsg)
+		return session, false
+	}
+
+	//source := getSource(session.SourceID)
+
+	return session, true
+}
+
+func (s *StreamServer) Setup(ctx context.Context, source *api.Source) (*api.Session, error) {
+	var session *api.Session
+
+	//Generate random IDs until we find one that isn't being used
+	//It'll be very rare the first one doesn't work
+	for {
+		sessionID := uuid.New().String()
+		if _, exists := s.getSession(sessionID); exists {
+			continue
+		} else {
+			session = &api.Session{
+				Id:     sessionID,
+				Source: source,
+			}
+			break
+		}
+	}
+
+	if err := s.setSession(session); err != nil {
+		emptySession := &api.Session{}
+		return emptySession, err
+	}
+
+	return session, nil
+}
+
+func (s *StreamServer) Play(ctx context.Context, source *api.Session) (*api.Result, error) {
+	return &api.Result{Successful: true}, nil
+}
+
+func (s *StreamServer) Pause(ctx context.Context, session *api.Session) (*api.Result, error) {
+	return &api.Result{Successful: true}, nil
+}
+
+func (s *StreamServer) Describe(ctx context.Context, source *api.Source) (*api.StreamSourceInfo, error) {
+	return &api.StreamSourceInfo{Id: "12345"}, nil
+}
+
+func (s *StreamServer) Teardown(ctx context.Context, session *api.Session) (*api.Result, error) {
+	if _, exists := s.getSession(session.Id); !exists {
+		errMsg := fmt.Sprintf("Could not find session with ID %s", session.Id)
+		return result("SessionNotFound", errMsg), errors.New(errMsg)
+	}
+
+	if err := s.delSession(session); err != nil {
+		return result("SessionNotDeleted", err.Error()), err
+	}
+
+	return result("", ""), nil
+}
+
+func (s *StreamServer) Status(ctx context.Context, source *api.Session) (*api.SessionStatus, error) {
+	return &api.SessionStatus{}, nil
+}
+
+func (s *StreamServer) Record(ctx context.Context, source *api.Source) (*api.Session, error) {
+	return &api.Session{Id: "12345"}, nil
+}
